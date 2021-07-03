7b5a3e57c78832a363388b9316b16c9ecbbef671
Oscar Lucas <olucas@espol.edu.ec>
2021-07-03 13:15:45 -0500

    Mejoras y lectura del archivo

--- mips1.asm
+++ mips1.asm
@@ -3,6 +3,9 @@ etiqueta: .asciiz "  *******CONVERSOR DE DIVISAS*******\n"
 saltoLinea: .asciiz "\n" 
 menu: .asciiz "- Cambio de USD - Dolar Estadounidense a : \n\t 1.EUR - Euro \n\t 2.GBP - Libra Esterlina \n\t 3.COP - Peso colombiano \n\t 4.CAD - Dolar Canadiense \n\t 5.XBT - Bitcoin \n\t 6.ETH - Ethereum"
 opcionEtiqueta: .asciiz "Elija una opcion: "
+opcionUsuarioEtiqueta: .asciiz "La opcion que usted escogio corresponde a la siguiente tarifa: "
+montoEtiqueta: .asciiz "\n\tCantidad de USD Americanos : "
+tarifaEtiqueta : .asciiz "\tIngrese la tarifa proporcionada : "
 fout: .asciiz "//Users//oscarlucas7//Conversor_De_Divisas//divisas.txt"
 buffer: .space 1000
 .text
@@ -37,11 +40,34 @@ main:
 	la $a1, buffer
 	jal leerArchivo
 	
-	
-	
 	move $a0, $s1
 	jal cerrarArchivo
-	li $v0,10
+	
+	
+	li $v0, 4
+	la $a0, saltoLinea
+	syscall
+	li $v0, 4
+	la $a0, montoEtiqueta
+	syscall
+	li $v0, 5
+	syscall
+	move $s2,$v0 #Monto se guarda en $s2
+	li $v0, 4
+	la $a0, saltoLinea
+	syscall
+	
+	
+	li $v0, 4
+	la $a0, tarifaEtiqueta
+	syscall
+	li $v0, 7 #tarifa se guarda en f0
+	syscall
+	li $v0, 4
+	la $a0, saltoLinea
+	syscall
+	
+	li $v0,10 #fin del programa
 	syscall
 	
 	
@@ -92,15 +118,19 @@ leerArchivo:
 	sw $s3,16($sp)
 	
 	
-	move $s0,$a3 #dia de cumpleanos
+	move $s0,$a3 #opcion de menu
 	move $s1,$a0 #fd
 	move $s2,$a1 #buffer
 	
 	li $t0,0 #contador de lineas
 	li $s3,10 #\n
 	
+	li $v0, 4
+	la $a0, opcionUsuarioEtiqueta
+	syscall
 	
 	leer:
+		
 		slt $t1,$t0,$s0
 		beq $t1,0, return
 		
@@ -117,6 +147,7 @@ leerArchivo:
 		
 		li $v0,4
 		move $a0,$s2
+		move $s5,$s2
 		syscall
 		j leer
 	
@@ -124,7 +155,10 @@ leerArchivo:
 		sumar:
 			addi $t0,$t0,1
 			j leer
-			
+		
+		li $v0, 4
+		move $a0,$s5
+		syscall
 	
 	return:
 	lw $ra, ($sp)
