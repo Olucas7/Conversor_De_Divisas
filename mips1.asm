0c2d2ccbafc3245dc3a5aee965a6bac9258e40f5
Oscar Lucas <olucas@espol.edu.ec>
2021-07-02 20:15:11 -0500

    Creacion del menu y el archivo con las tarifas

--- mips1.asm
+++ mips1.asm
@@ -1,7 +1,8 @@
 .data 
-etiqueta: .asciiz "*******CONVERSOR DE DIVISAS*******\n"
+etiqueta: .asciiz "  *******CONVERSOR DE DIVISAS*******\n"
 saltoLinea: .asciiz "\n" 
 menu: .asciiz "- Cambio de USD - Dolar Estadounidense a : \n\t 1.EUR - Euro \n\t 2.GBP - Libra Esterlina \n\t 3.COP - Peso colombiano \n\t 4.CAD - Dolar Canadiense \n\t 5.XBT - Bitcoin \n\t 5.ETH - Ethereum"
+opcionEtiqueta: .asciiz "Elija una opcion: "
 .text
 .globl main
 
@@ -19,6 +20,9 @@ main:
 	li $v0, 4
 	la $a0, saltoLinea
 	syscall
+	li $v0, 4
+	la $a0, opcionEtiqueta
+	syscall
 	#li $t0,20
 	#sll $t0,$t0,2
 	#addi $t0, $t0,-1
@@ -26,5 +30,8 @@ main:
 	#li $v0,1
 	#syscall
 	
-	#li $v0,10
-	#syscall
+	
+	
+	
+	li $v0,10
+	syscall
